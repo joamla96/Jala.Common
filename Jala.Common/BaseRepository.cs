bcb3b337284b1a0d8499a8678aff540a7c0dc135
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-16 08:58:38 +0100

    Base Repositories

---
+++ Jala.Common/BaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Jala.Common
+{
+    public class BaseRepository<T>
+    {
+        private List<T> repository = new List<T>();
+
+        public virtual void Add(T item)
+        {
+            repository.Add(item);
+        }
+
+        public virtual void Remove(T item)
+        {
+            repository.Remove(item);
+        }
+
+        public virtual List<T> GetAll(bool AsCopy = false)
+        {
+            if (AsCopy)
+            {
+                return repository.ToList<T>();
+            }
+            else return repository;
+        }
+    }
+}
---
+++ Jala.Common/BaseRepositorySingleton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jala.Common
+{
+    class BaseRepositorySingleton<T> : BaseRepository<T>
+    {
+        private BaseRepositorySingleton<T> instance;
+        public BaseRepositorySingleton<T> Instace { get {
+                if(this.instance == null) { this.instance = new BaseRepositorySingleton<T>(); }
+                return this.instance;
+            }
+        }
+    }
+}
