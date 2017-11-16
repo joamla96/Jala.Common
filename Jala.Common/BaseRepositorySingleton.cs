dbd9d03dc9b932b301a3b74ad58f0289e0c307a5
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-16 09:27:54 +0100

    Singletons dont seem to work like this...

--- Jala.Common/BaseRepositorySingleton.cs
+++ Jala.Common/BaseRepositorySingleton.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Jala.Common
-{
-    class BaseRepositorySingleton<T> : BaseRepository<T>
-    {
-        private BaseRepositorySingleton<T> instance;
-        public BaseRepositorySingleton<T> Instace { get {
-                if(this.instance == null) { this.instance = new BaseRepositorySingleton<T>(); }
-                return this.instance;
-            }
-        }
-    }
-}
+//namespace Jala.Common
+//{
+//    public class BaseRepositorySingleton<T> : BaseRepository<T>
+//    {
+//        private BaseRepositorySingleton<T> instance;
+//        public BaseRepositorySingleton<T> Instace { get {
+//                if (this.instance == null) { this.instance = new BaseRepositorySingleton<T>(); }
+//                return this.instance;
+//            }
+//        }
+
+//        //private BaseRepositorySingleton() { }
+//    }
+//}
