4d89e46265b0e7e9b0ae29ec3f5db484c20995ba
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-30 09:49:32 +0100

    Observer patterns

---
+++ Jala.Common/Observer/Observable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jala.Common.Observer
+{
+    public abstract class Observable
+    {
+		private List<IObserver> Observers = new List<IObserver>();
+
+		public void Subscribe(IObserver i)
+		{
+			Observers.Add(i);
+		}
+
+		public void Unsubscribe(IObserver i)
+		{
+			Observers.Remove(i);
+		}
+
+		public void Notify(string[] args = null)
+		{
+			foreach(IObserver o in Observers)	{
+				o.Update(args);
+			}
+		}
+    }
+}
